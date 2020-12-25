d7a73ea6848df919d8a244ef310ade5971a4e585
Sandro0330 <sandro030486@gmail.com>
2020-12-25 19:45:19 -0300

    Operador Unário (Unary Operator)

--- src/br/com/java/lambdas/Fornecedor.java
+++ src/br/com/java/lambdas/Fornecedor.java
@@ -1,2 +1,13 @@
-package br.com.java.lambdas;public class Fornecedor {
+package br.com.java.lambdas;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+    public static void main(String[] args) {
+        Supplier< List<String> > umaLista = () -> Arrays.asList("Sandro", "Regina", "Uilson", "Monaliza");
+        System.out.println(umaLista.get());
+    }
 }
